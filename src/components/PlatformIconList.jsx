40e1f1a31be11d8bd24bbfa8475162f4318e0bc2
Rahul Darla <darla.rahul@gmail.com>
2023-03-21 01:30:33 +0530

    displaying platform icons

---
+++ src/components/PlatformIconList.jsx
@@ -0,0 +1,37 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+import { HStack, Icon } from "@chakra-ui/react";
+
+const PlatformIconList = ({ platforms }) => {
+  const icons = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={"20px"}>
+      {platforms.map((platform) => (
+        <Icon as={icons[platform.slug]} color="teal.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
