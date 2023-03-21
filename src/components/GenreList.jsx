b568736cb7ce410ec9fa04c6f67900eaee27eae3
Rahul Darla <darla.rahul@gmail.com>
2023-03-21 23:33:30 +0530

    sidebar genres images

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,15 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import React from "react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { genres } = useGenres();
   return (
     <div>
-      <ul>
+      <List>
         {genres.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
