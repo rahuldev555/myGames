86859b42940303a3f0375b9c0645329113a5ca13
Rahul Darla <darla.rahul@gmail.com>
2023-03-21 11:11:14 +0530

    image cropped to min size

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,12 +1,13 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
 const GameCard = ({ game }) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
