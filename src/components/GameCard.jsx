0940a47b78d7f4169b628811436c008ce1265c01
Rahul Darla <darla.rahul@gmail.com>
2023-04-14 17:39:37 +0530

    gameCardContainer is added

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -6,7 +6,7 @@ import PlatformIconList from "./PlatformIconList";
 
 const GameCard = ({ game }) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
