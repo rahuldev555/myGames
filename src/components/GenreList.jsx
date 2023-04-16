c54674927a86ec12821a8157bf42df84dff8468c
Rahul Darla <darla.rahul@gmail.com>
2023-04-16 20:12:14 +0530

    cleanup the genres

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,21 +1,35 @@
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = ({ selectedGenre, onSelectGenre }) => {
   const { data, isLoading, error } = useGenres();
   return (
-    <div>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
+                objectFit="cover"
                 boxSize="32px"
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
+                whiteSpace={"normal"}
+                textAlign="left"
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
                 variant="link"
@@ -27,7 +41,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }) => {
           </ListItem>
         ))}
       </List>
-    </div>
+    </>
   );
 };
 
