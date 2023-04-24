3d3a20f71ddcfc9197a27fdc7bd0be58dd6aec7d
Rahul Darla <darla.rahul@gmail.com>
2023-04-24 18:44:56 +0530

    dark color mode is applied

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import { HStack, Icon } from "@chakra-ui/react";
 import { SiAmazongames } from "react-icons/si";
 import SearchInput from "./SearchInput";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <>
-      <HStack padding="10px">
-        <Icon as={SiAmazongames} boxSize="80px" />
+      <HStack padding="20px" justifyContent="space-between">
+        <Icon as={SiAmazongames} boxSize="70px" />
         <SearchInput />
+        <ColorModeSwitch />
       </HStack>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import theme from "./theme";
 
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -11,7 +12,8 @@ import router from "./routes";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools />
