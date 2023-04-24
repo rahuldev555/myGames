bb2b364a55bea32d05b6879e5559d1585528a694
Rahul Darla <darla.rahul@gmail.com>
2023-04-24 18:49:43 +0530

    some code modified

--- src/components/SideBar.jsx
+++
@@ -1,8 +0,0 @@
-import { IconButton, Menu, MenuButton, MenuList } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
-
-const SideBar = () => {
-  return <></>;
-};
-
-export default SideBar;
