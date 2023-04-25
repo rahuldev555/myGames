6f255e93293a7e61f1dd9e2c1f544d4997959c76
Rahul Darla <darla.rahul@gmail.com>
2023-04-25 11:15:04 +0530

    i think app is completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,17 @@ import { HStack, Icon } from "@chakra-ui/react";
 import { SiAmazongames } from "react-icons/si";
 import SearchInput from "./SearchInput";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <>
-      <HStack padding="20px" justifyContent="space-between">
+    <HStack padding="20px" justifyContent="space-between">
+      <Link to="/">
         <Icon as={SiAmazongames} boxSize="70px" />
-        <SearchInput />
-        <ColorModeSwitch />
-      </HStack>
-    </>
+      </Link>
+      <SearchInput />
+      <ColorModeSwitch />
+    </HStack>
   );
 };
 
